8e78a1f18265fee80a2abee609d300f4fbf764fe
bali777-slot <152234441+bali777-slot@users.noreply.github.com>
2023-11-28 08:55:04 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,28 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Bali777</Title>
+
+      <h1>Disini anda bisa menemukan Link Alternatif Bali777</h1>
+      <!-- This is a comment in the source -->
+<center>
+<video width="320" height="240" controls>
+  <source src="https://bali777.biz/wp-content/uploads/2023/11/Kontak_bali777.mp4" type="video/mp4">
+
+</video>
+</center>
+<h3>Selamat Datang di  <span style="background-color: #914c53; color: #ffffff; padding: 0 3px;">BALI777</span></h3>
+<p><strong>Situs Bali777 melayani member dalam bertransaksi dengan aman dan terpercaya di Indonesia, Bali777 juga memiliki banyak bonus yang dapat di nikmati dalam bermain  dengan sesama member <a href="https://bali777.biz/">Bali777</a></p>.</strong></p>
+<p>Bali777 adalah Agen Slot Online paling Gacor di Indonesia yang menyediakan ratusan game slot terpopuler dengan Win Rate Terbaik.  <img src="https://htmlcodeeditor.com/images/smiley.png" alt="smiley" /></p>
+<center>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126220.015916504!2d115.01832644335941!3d-8.655791599999981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd247e9922eeec7%3A0xed3f025f4303428a!2sbali777!5e0!3m2!1sid!2sid!4v1701134566573!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </center>
+<p>
+  
+</p>
+<p>  Jangan Lupa Review <a href="https://search.google.com/local/writereview?placeid=ChIJx-4ukulH0i0RikIDQ18CP-0/">Bali777</a></p>
+
+<p>  Daftar/Registrasi di >>> <a href="https://bursa4d.lol/account/register">DAFTAR</a></p>
       <Counter />
       <p>
         Visit{" "}
